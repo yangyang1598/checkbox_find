d0bfa69d83d68ca06022192bee31f8623841a761
양진현 <92699138+yangyang1598@users.noreply.github.com>
2021-10-18 12:18:36 +0900

    second commit

---
+++ 체크박스 검출.py
@@ -0,0 +1,41 @@
+import numpy as np
+import cv2
+import glob
+import matplotlib.pyplot as plt
+# %matplotlib qt
+
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((6*9,3), np.float32)
+objp[:,:2] = np.mgrid[0:9, 0:6].T.reshape(-1,2)
+
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d points in real world space
+imgpoints = [] # 2d points in image plane.
+
+# Make a list of calibration images
+images = glob.glob('yang/KaKaoTalk*.jpg')
+
+# Step through the list and search for chessboard corners
+for idx, fname in enumerate(images):
+    img = cv2.imread(fname)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # img = cv2.resize(img, dsize=(720, 1280), interpolation=cv2.INTER_AREA)
+    # Find the chessboard corners
+    ret, corners = cv2.findChessboardCorners(gray, (9,6), None)
+
+    # If found, add object points, image points
+    if ret == True:
+        objpoints.append(objp)
+        imgpoints.append(corners)
+
+        # Draw and display the corners
+        cv2.drawChessboardCorners(img, (9,6), corners, ret)
+        write_name = 'new_store'+str(idx)+'.jpg'
+        cv2.imwrite(write_name, img)
+
+        cv2.imshow('img', img)
+        cv2.waitKey(40)
+
+
+cv2.waitKey(100)
+cv2.destroyAllWindows()
